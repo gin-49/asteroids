1f14a0e9416b303eb4b968bd2c2673376ddc9d7e
gin <49gin49@gmail.com>
2025-07-23 15:39:02 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+from constants import *
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
